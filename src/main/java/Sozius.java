7d1d4bca7b55a2f3829ccb1bf0be7f116476267a
Ye Xintai <254204367+YeXintai@users.noreply.github.com>
2026-08-16 17:19:34 +0800

    Implement Level 2
    Add capability to add tasks
    Add capability to list tasks

--- src/main/java/Sozius.java
+++ src/main/java/Sozius.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Sozius {
-    public static final String sep = "_________________________________________________________________\n";
+    private static final String sep = "_________________________________________________________________\n";
 
     public static void main(String[] args) {
         String banner =
@@ -14,18 +14,18 @@ public class Sozius {
             "       |\\_________\\|_______|\\|_______|\\|__|\\|_______|\\_________\\\n" +
             "       \\|_________|                                 \\|_________|\n" +
             "                                                                \n";
-
         String greeting =
                 sep +
                 banner +
                 "Sozius: Hello! I'm Sozius.\n" +
                 "        What do you need?\n" +
                 sep;
-
         String goodbye =
                 sep +
                 "Sozius: Goodbye.\n" +
                 sep;
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println(greeting);
         Scanner input = new Scanner(System.in);
@@ -33,10 +33,18 @@ public class Sozius {
             String line = input.nextLine();
             if (line.equals("bye")) {
                 break;
+            } else if (line.equals("list")) {
+                System.out.print(sep);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " +  tasks[i]);
+                }
+                System.out.print(sep);
+            } else {
+                tasks[taskCount++] = line;
+                System.out.print(sep);
+                System.out.println("added: " + line);
+                System.out.print(sep);
             }
-            System.out.print(sep);
-            System.out.println("Sozius: " + line);
-            System.out.print(sep);
         }
         System.out.println(goodbye);
     }
