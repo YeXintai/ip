17ff36586550eb0717104fb6bb0961cddaa7a565
Ye Xintai <254204367+YeXintai@users.noreply.github.com>
2026-08-16 20:21:50 +0800

    Add deadline and event task classes

---
+++ src/main/java/EventTask.java
@@ -0,0 +1,15 @@
+public class EventTask extends Task {
+    private String from;
+    private String to;
+
+    public EventTask(String desc, String from, String to) {
+        super(desc);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[D]%s (from: %s to: %s)", super.toString(), from, to);
+    }
+}
