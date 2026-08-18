57957e10185b8e41108e381e4983c523dd13f871
Ye Xintai <254204367+YeXintai@users.noreply.github.com>
2026-08-18 12:57:23 +0800

    Add error handling and refactor command parsing
    Added error handling for the 2 types of errors mentioned
    Refactor input parsing into separate function, and moved each command into its own function

--- src/main/java/Sozius.java
+++ src/main/java/Sozius.java
@@ -6,6 +6,86 @@ import java.util.*;
 
 public class Sozius {
     private static final String sep = "_________________________________________________________________\n";
+    private static Task[] tasks = new Task[100];
+    private static int taskCount = 0;
+
+    private static void markTask(String args) {
+        int index = Integer.parseInt(args);
+        tasks[index - 1].setDone(true);
+        System.out.println("Marked as done:");
+        System.out.println(tasks[index - 1]);
+    }
+    private static void unmarkTask(String args) {
+        int index = Integer.parseInt(args);
+        tasks[index - 1].setDone(false);
+        System.out.println("Marked as not done:");
+        System.out.println(tasks[index - 1]);
+    }
+    private static void createTodoTask(String args) {
+        if (args.equals("")) {
+            System.out.println("Invalid command: incorrect number of arguments for todo");
+            return;
+        }
+        tasks[taskCount++] = new TodoTask(args);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(tasks[taskCount - 1]);
+        System.out.println("Now you have " + taskCount + " tasks in the list");
+    }
+    private static void createDeadlineTask(String args) {
+        String[] splitArgs = args.split(" /by ");
+        String desc = splitArgs[0];
+        String deadline = splitArgs[1];
+        tasks[taskCount++] = new DeadlineTask(desc, deadline);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(tasks[taskCount - 1]);
+        System.out.println("Now you have " + taskCount + " tasks in the list");
+    }
+    private static void createEventTask(String args) {
+        String[] splitArgs1 = args.split(" /from ");
+        String desc = splitArgs1[0];
+        String[] splitArgs2 = splitArgs1[1].split(" /to ");
+        String from = splitArgs2[0];
+        String to = splitArgs2[1];
+        tasks[taskCount++] = new EventTask(desc, from, to);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(tasks[taskCount - 1]);
+        System.out.println("Now you have " + taskCount + " tasks in the list");
+    }
+
+    private static void parse(String line) {
+        if (line.equals("list")) {
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println((i + 1) + ". " +  tasks[i]);
+            }
+        }
+        int firstSpace = line.indexOf(' ');
+        if (firstSpace == -1) {
+            System.out.println("Invalid command: Commands other than list require at least 1 argument");
+            return;
+        }
+        String command  = firstSpace == -1 ? line : line.substring(0, firstSpace);
+        String args = firstSpace == -1 ? line : line.substring(firstSpace + 1);
+        switch (command) {
+            case "mark":
+                markTask(args);
+                break;
+            case "unmark":
+                unmarkTask(args);
+                break;
+            case "todo":
+                createTodoTask(args);
+                break;
+            case "deadline":
+                createDeadlineTask(args);
+                break;
+            case "event":
+                createEventTask(args);
+                break;
+            default:
+                System.out.println("Error: unknown command");
+                break;
+        }
+    }
 
     public static void main(String[] args) {
         String banner =
@@ -27,64 +107,16 @@ public class Sozius {
         String goodbye =
                 "Sozius: Goodbye.\n" +
                 sep;
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
 
         System.out.println(greeting);
         Scanner input = new Scanner(System.in);
         while (true) {
             String line = input.nextLine();
             System.out.print(sep);
-            int firstSpace = line.indexOf(' ');
-            String command  = firstSpace == -1 ? line : line.substring(0, firstSpace);
-            if (command.equals("bye")) {
+            if (line.equals("bye")) {
                 break;
-            } else if (command.equals("list")) {
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " +  tasks[i]);
-                }
-            } else if (firstSpace == -1) {
-                System.out.println("Invalid command: " + command);
-            } else if (command.equals("mark")) {
-                String line_args = line.substring(firstSpace + 1);
-                int index = Integer.parseInt(line_args);
-                tasks[index - 1].setDone(true);
-                System.out.println("Marked as done:");
-                System.out.println(tasks[index - 1]);
-            } else if (command.equals("unmark")) {
-                String line_args = line.substring(firstSpace + 1);
-                int index = Integer.parseInt(line_args);
-                tasks[index - 1].setDone(false);
-                System.out.println("Marked as not done:");
-                System.out.println(tasks[index - 1]);
-            } else if (command.equals("todo")) {
-                String line_args = line.substring(firstSpace + 1);
-                tasks[taskCount++] = new TodoTask(line_args);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[taskCount - 1]);
-                System.out.println("Now you have " + taskCount + " tasks in the list");
-            } else if (command.equals("deadline")) {
-                String line_args = line.substring(firstSpace + 1);
-                String[] splitArgs = line_args.split(" /by ");
-                String desc = splitArgs[0];
-                String deadline = splitArgs[1];
-                tasks[taskCount++] = new DeadlineTask(desc, deadline);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[taskCount - 1]);
-                System.out.println("Now you have " + taskCount + " tasks in the list");
-            } else if (command.equals("event")) {
-                String line_args = line.substring(firstSpace + 1);
-                String[] splitArgs1 = line_args.split(" /from ");
-                String desc = splitArgs1[0];
-                String[] splitArgs2 = splitArgs1[1].split(" /to ");
-                String from = splitArgs2[0];
-                String to = splitArgs2[1];
-                tasks[taskCount++] = new EventTask(desc, from, to);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[taskCount - 1]);
-                System.out.println("Now you have " + taskCount + " tasks in the list");
             } else {
-                System.out.println("Invalid command: unknown command");
+                parse(line);
             }
             System.out.print(sep);
         }
